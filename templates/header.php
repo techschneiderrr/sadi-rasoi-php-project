703f5b6d9ab0fa6a9658b4ddd87b3c068c0a7989
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2021-01-21 20:09:08 +0530

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -11,7 +11,7 @@
         .brand-text {
             color: #cbb09c !important;
         }
-ergdfewgrdfbcv 
+ergdfewgrdfbcv regdbfgbv
         form {
             max-width: 460px;
             margin: 20px auto;
