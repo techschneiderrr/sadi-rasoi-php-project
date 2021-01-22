6032cf083d6f44d4636b78761806f149434209c9
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2021-01-22 21:01:23 +0530

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -28,4 +28,4 @@
                 <li><a href="add.php" class="btn brand z-depth-0">Add a Dish</a></li>
             </ul>
         </div>
-    </nav>5ter43t4t3ry45rt5y4try5trfty45tg45trftrftgdfy6trgrtryfggrfvtrhgftghfthrgftgb
+    </nav>5ter43t4t3ry45rt5y4try5trfty45tg45trftrftgdfy6trgrtryfggrfvtrhgftghfthrgftgbgcv
